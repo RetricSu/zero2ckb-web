4f152be456b4862c3b31739d297d53daab602a98
RetricSu <inneverland2013@gmail.com>
2021-01-06 14:34:16 +0800

    ready to push online

--- src/components/tutorial/sections/common/TotalCapacity.tsx
+++ src/components/tutorial/sections/common/TotalCapacity.tsx
@@ -90,7 +90,7 @@ export default function TotalCapacity (props: TotalCapacityProps) {
             cell_output: {
                 capacity: sum,
                 lock: {
-                    code_hash: '',
+                    code_hash: '0x9bd7e06f3ecf4be0f2fcd2188b23f1b9fcc88e5d4b65a8637b17723bbda3cce8',
                     hash_type: 'type',
                     args: '',
                 },
