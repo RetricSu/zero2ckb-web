809f1e695884b4813d9c60bb4e691ef64bb74462
ChemaESP <107578801+ChemaESP@users.noreply.github.com>
2022-06-17 12:07:58 +0200

    Update i18n.ts

--- src/i18n.ts
+++ src/i18n.ts
@@ -3,6 +3,7 @@ import { initReactI18next } from "react-i18next";
 import LanguageDetector from "i18next-browser-languagedetector";
 import enTrans from "./locales/en.json";
 import zhTrans from "./locales/zh.json";
+import esTrans from "./locales/es.json";
 
 const resources = {
   en: {
@@ -11,6 +12,9 @@ const resources = {
   zh: {
     translation: zhTrans,
   },
+  es: {
+    translation: esTrans,
+     },
 };
 
 i18n
